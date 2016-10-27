01e1998caafa27f35cbaf19dd1262930dd191f24
Dennis Hatwieger <hej@dennishatwieger.de>
2016-10-27 14:49:56 +0200

    save

--- src/components/atoms/atom-timing.vue
+++ src/components/atoms/atom-timing.vue
@@ -1,28 +1,21 @@
 <template name="atomTiming">
-	<div class="atom__timing">
-			<h3>Timing:</h3>
-			<div class="atom__duration" v-on:submit.prevent >
-				<span>Duration:</span>
-				<input v-model="fadeDuration" @keyup.enter="saveNumber" type="number" placeholder="1000ms" />
-				<input type="text" style="display:none;">
-			</div>
-	</div>
+
+  <input :atom-duration="value" v-model="duration"  @input="$emit('input', $event.target.value)"  type="number" />
+
 </template>
 
 <script>
 export default {
   name: 'atomTiming',
+  props: ['value'],
   data () {
     return {
-      fadeDuration: "",
+      duration: ""
     }
   },
-		methods:{
-			saveNumber: function(){
-				console.log(this.fadeDuration)
-				this.$emit("this.fadeDuration")
-				this.saveNumber = ""
-			}
-		}
+  created: function() {
+      this.duration = this.value
+  }
+
 }
 </script>
--- src/components/atoms/atom.vue
+++ src/components/atoms/atom.vue
@@ -1,4 +1,4 @@
-<template name="atom">
+easeFunction<template name="atom">
     <div class="atom__shell">
       <form class="atom__name" v-on:submit.prevent>
         <input type="" name="atome_nameChange" v-model="atomName" placeholder="Name"/>
@@ -13,35 +13,46 @@
         >
           <div class="atom__nukleolus" v-if="nukleolus"></div>
          </transition>
-
       </div>
+
       <div class="atom__properties">
+
+      <select v-model="selected" class="atom__kind">
+           <option v-for="option in options" v-bind:value="option.value" >
+             {{ option.text }}
+           </option>
+      </select>
+
+
+
       <form class="atom__behavior">
-        <div class="atom__kind">
-          <option v-for="option in options" :value="option.value" />
+        <h3>timing</h3>
+        <div v-for="timing in timings">
+          <atomTiming v-model="timing.value"></atomTiming>
         </div>
-        <div class="atom__timing">
-          <h3> timing </h3>
-          <input type="number" v-model="atomDuration" />
-          <input type="number" v-model="atomDelay">
-        </div>
+
+
         <div class="atom__characteristics">
          <h3>characteristics</h3>
-         <input type="number" v-model="atomWidth" />
-         <input type="number" v-model="atomHeight">
+
+         <!-- size -->
+         <span v-if="selected == 'atom__size'">
+           <input type="number"  v-model="atomWidth" />
+           <input type="number" v-model="atomHeight">
+         </span>
+
         </div>
       </form>
-
       </div>
     </div>
+
 </template>
 
 <script>
 import atomTiming from './atom-timing.vue'
 
 
-
-  export default {
+ export default {
     name: "atom",
     components: {
       atomTiming
@@ -53,30 +64,88 @@ import atomTiming from './atom-timing.vue'
         atomWidth: "20",
         atomHeight: "20",
         nukleolus: true,
-        duration: 1000
-
+        selected: "atom__size",
+        easings: [
+          { easeFunction: 'linear' },
+          { easeFunction: 'easeInSine' },
+          { easeFunction: 'easeOutSine' },
+          { easeFunction: 'easeInOutSine' },
+          { easeFunction: 'easeInQuad' },
+          { easeFunction: 'easeOutQuad' },
+          { easeFunction: 'easeInOutQuad' },
+          { easeFunction: 'easeInCubic' },
+          { easeFunction: 'easeOutCubic' },
+          { easeFunction: 'easeInQuart' },
+          { easeFunction: 'easeOutQuart' },
+          { easeFunction: 'easeInOutQuart' },
+          { easeFunction: 'easeInQuint' },
+          { easeFunction: 'easeOutQuint' },
+          { easeFunction: 'easeInOutQuint' },
+          { easeFunction: 'easeInExpo' },
+          { easeFunction: 'easeOutExpo' },
+          { easeFunction: 'easeInOutExpo' },
+          { easeFunction: 'easeInCirc' },
+          { easeFunction: 'easeOutCirc' },
+          { easeFunction: 'easeInOutCirc' },
+          { easeFunction: 'easeInBack' },
+          { easeFunction: 'easeOutBack' },
+          { easeFunction: 'easeInOutBack' },
+          { easeFunction: 'easeInElastic' },
+          { easeFunction: 'easeOutElastic' },
+          { easeFunction: 'easeInOutElastic' },
+          { easeFunction: 'easeInBounce' },
+          { easeFunction: 'easeOutBounce' },
+          { easeFunction: 'easeInOutBounce' },
+        ],
+        options: [
+            { text: 'size', value: 'atom__size' },
+            { text: 'rotation', value: 'atom__rotation' },
+            { text: 'transition', value: 'atom__transition' },
+            { text: 'opacity', value: 'atom__opacity' },
+            { text: 'color', value: 'atom__color' }
+          ],
+        timings: [
+          { kind: 'atomDuration', value: "1000" },
+          { kind: 'atomDelay', value: "0" },
+        ]
       }
     },
     mounted: function () {
       this.nukleolus = false
     },
-    
-    methods: {
+    computed: {
+      atomDuration: function(){
+        for (var i = 0; i < this.timings.length; i++) {
+          if (this.timings[i].kind == "atomDuration"){
+            return this.timings[i].value
+          }
+        }
+      },
 
+      atomDDelay: function(){
+        for (var i = 0; i < this.timings.length; i++) {
+          if (this.timings[i].kind == "atomDelay"){
+            return this.timings[i].value
+          }
+        }
+      }
+
+    },
+    methods: {
       beforeEnter: function (el) {
         el.style.width = '2vw'
         el.style.height = '2vw'
-    },
+        },
       enter: function (el, done) {
         var vm = this
         Velocity(el,
           { width: this.atomWidth,
            height: this.atomHeight ,
-           translateX: "200px"
+           //translateX: "200px"
           },
           {
            easing: "easeInSine",
-            duration: this.duration,
+            duration: this.atomDuration,
             complete: function () {
               done()
               if (!vm.stop) vm.nukleolus = false
@@ -90,7 +159,7 @@ import atomTiming from './atom-timing.vue'
           { width: '2vw',
             height: '2vw' },
           {
-            duration: this.duration,
+            duration: this.atomDuration,
             complete: function () {
               done()
               vm.nukleolus = true
@@ -98,6 +167,7 @@ import atomTiming from './atom-timing.vue'
           }
         )
       }
-    }
+    },
+
   }
 </script>
